32871635a4e2d7ec194e9aecda2e91288ecc57ef
Andrew Steinborn <git@steinborn.me>
2020-07-13 09:06:48 -0400

    Add chat prefixes to messages.

--- common/src/main/java/me/steinborn/shout/logic/AnnouncerTask.java
+++ common/src/main/java/me/steinborn/shout/logic/AnnouncerTask.java
@@ -4,17 +4,23 @@ import com.google.inject.Inject;
 import me.steinborn.shout.announcement.Announcement;
 import me.steinborn.shout.announcement.chain.AnnouncementChain;
 import me.steinborn.shout.announcement.chain.AnnouncementChainStorage;
+import me.steinborn.shout.config.ConfigurationHolder;
 import me.steinborn.shout.platform.ShoutPlatform;
 import me.steinborn.shout.platform.ShoutPlayer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 
 public class AnnouncerTask implements Runnable {
     private final ShoutPlatform<?> platform;
+    private final ConfigurationHolder configurationHolder;
     private final AnnouncementChainStorage chainStorage;
 
     @Inject
     public AnnouncerTask(ShoutPlatform<?> platform,
+                         ConfigurationHolder configurationHolder,
                          AnnouncementChainStorage chainStorage) {
         this.platform = platform;
+        this.configurationHolder = configurationHolder;
         this.chainStorage = chainStorage;
     }
 
@@ -26,7 +32,14 @@ public class AnnouncerTask implements Runnable {
             if (announcement == null) {
                 continue;
             }
-            player.sendMessage(announcement.content());
+            player.sendMessage(formatAnnouncement(announcement, player));
         }
+    }
+
+    private Component formatAnnouncement(Announcement announcement, ShoutPlayer player) {
+        return TextComponent.builder()
+                .append(configurationHolder.config().getPrefix())
+                .append(announcement.content())
+                .build();
     }
 }
